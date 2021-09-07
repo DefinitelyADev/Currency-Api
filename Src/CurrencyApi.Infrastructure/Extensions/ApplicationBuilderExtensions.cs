f17873bfbf07658ebb88b2f4fec483c753570f50
Ilias Tsakiridis <ilias.tsakiridis@outlook.com>
2021-09-07 08:26:44 +0300

    implement ConfigureRequestPipeline extension method and partial StartEngine

--- Src/CurrencyApi.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ Src/CurrencyApi.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CurrencyApi.Infrastructure.Core.Engine;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 
 namespace CurrencyApi.Infrastructure.Extensions
 {
@@ -10,11 +12,25 @@ namespace CurrencyApi.Infrastructure.Extensions
         /// <param name="application">Builder for configuring an application's request pipeline</param>
         public static void ConfigureRequestPipeline(this IApplicationBuilder application)
         {
-            // EngineContext.Current.ConfigureRequestPipeline(application);
+            EngineContext.Current.ConfigureRequestPipeline(application);
         }
-        
+
         public static void StartEngine(this IApplicationBuilder application)
         {
+            var engine = EngineContext.Current;
+
+            //further actions are performed only when the database is installed
+            if (DataSettingsManager.IsDatabaseInstalled())
+            {
+
+
+#if !DEBUG      //prevent save the update migrations into the DB during the developing process
+                var migrationManager = EngineContext.Current.Resolve<IMigrationManager>();
+                migrationManager.MigrateUpAsync().Wait();
+#endif
+                //log application start
+                engine.Resolve<ILogger<EngineContext>>().LogInformation("Application started");
             }
+        }
     }
 }
